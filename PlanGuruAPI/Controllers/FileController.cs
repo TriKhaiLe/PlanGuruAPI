3ede83517ddc7eec539e8d3787ab100dbc4148bb
kimthu09 <21521495@gm.uit.edu.vn>
2025-05-08 20:39:16 +0700

    feat: handle save file

---
+++ PlanGuruAPI/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Files.Command;
+using Application.Files.Command.SaveFile;
+using Application.Files.Common;
+using Application.Files.Common.SaveFile;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanGuruAPI.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController(ISender mediator) : ControllerBase
+    {
+        [HttpPost("upload-file")]
+        public async Task<IActionResult> UploadFile([FromForm] SaveFileRequest request)
+        {
+            var result = await mediator.Send(new SaveFileCommand(request));
+            return Ok(result);
+        }
+    }
+}
